81af5d343496682c8d912459a193acb399f44fb2
pelatge <pelat.g29@gmail.com>
2020-10-10 17:00:47 +0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Home from './home';
+import Login from './login';
+import Register from './register';
+
+
+
+
+
+
+
+
+export {Home,Login,Register};
---
+++ src/pages/register/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Register = () => {
+	return (
+		<div>
+		<div>
+		<p>ilustrasi</p>
+		</div>
+		<div>
+		<p>Form Register</p>
+		</div>
+		</div>
+		)
+}
+export default Register;
